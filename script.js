e0e9dea8f693cbf96974b81cf19d2b523d5e4ab3
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-10-20 10:34:09 +0300

    MyMath object

--- script.js
+++ script.js
@@ -2,58 +2,64 @@ const MyMath = {
 	PI: 3.14159265359,
 
 	pow: function (number, degree) {
-		if (isNaN(number) || isNaN(degree) || typeof number !== 'number' || typeof degree !== 'number') {
-			return 'Error: Please, enter a numeric value for number and power.';
+		if (isNaN(number) || isNaN(degree) || !isFinite(number) || !isFinite(degree)) {
+			return 'Error: Please, enter a numbers.';
 		}
 		let result = 1;
-		for (let i = 0; i < degree; i++) {
+		for (let i = 0; i < (degree < 0 ? -degree : degree); i++) {
 			result *= number;
 		}
 		return degree < 0 ? 1 / result : result;
 	},
 
 	abs: function (number) {
-		if (isNaN(number) || typeof number !== 'number') {
-			return 'Error: Please, enter a numeric value.';
+		if (isNaN(number) || !isFinite(number)) {
+			return 'Error: Please, enter a number.';
 		}
 		return number < 0 ? -number : number;
 	},
 
-	max: function () {
-		if (arguments.length === 0) {
+	max: function (...args) {
+		if (args.length === 0) {
 			return 'Error: Please, enter numbers.';
 		}
 		let maxNumber = Number.NEGATIVE_INFINITY;
-		for (let i = 0; i < arguments.length; i++) {
-			if (isNaN(arguments[i]) || typeof arguments[i] !== 'number') {
+		for (let i = 0; i < args.length; i++) {
+			if (isNaN(args[i]) || !isFinite(args[i])) {
 				return 'Error: Please, enter numeric value for all numbers.';
 			}
-			if (arguments[i] > maxNumber) {
-				maxNumber = arguments[i];
+			if (args[i] > maxNumber) {
+				maxNumber = args[i];
 			}
 		}
 		return maxNumber;
 	},
 
-	min: function () {
-		if (arguments.length === 0 || arguments.length === '') {
+	min: function (...args) {
+		if (args.length === 0) {
 			return 'Error: Please, enter a numbers.';
 		}
 		let minNumber = Number.POSITIVE_INFINITY;
-		for (let i = 0; i < arguments.length; i++) {
-			if (isNaN(arguments[i]) || typeof arguments[i] !== 'number') {
+		for (let i = 0; i < args.length; i++) {
+			if (isNaN(args[i]) || !isFinite(args[i])) {
 				return 'Error: Please, enter numeric value for all numbers.';
 			}
-			if (arguments[i] < minNumber) {
-				minNumber = arguments[i];
+			if (args[i] < minNumber) {
+				minNumber = args[i];
 			}
 		}
 		return minNumber;
 	}
 };
 
-console.log(MyMath.PI);
-console.log(MyMath.pow(5, 3));
-console.log(MyMath.abs(-7));
-console.log(MyMath.max(10, 5, 18, 15));
-console.log(MyMath.min(12, 7, 3, 9));
+console.log(`MyMath.PI: ${MyMath.PI}`);
+console.log(`MyMath.pow(): ${MyMath.pow()}`);
+console.log(`MyMath.pow(5, 'j'): ${MyMath.pow(5, 'j')}`);
+console.log(`MyMath.pow(5, -3): ${MyMath.pow(5, -3)}`);
+console.log(`MyMath.abs(): ${MyMath.abs()}`);
+console.log(`MyMath.abs('j'): ${MyMath.abs('j')}`);
+console.log(`MyMath.abs(-7): ${MyMath.abs(-7)}`);
+console.log(`MyMath.max(): ${MyMath.max()}`);
+console.log(`MyMath.max(10, 5, 18, 'j', 25, 12): ${MyMath.max(10, 5, 18, 'j', 25, 12)}`);
+console.log(`MyMath.max(10, 5, 18, 3, 25, 12): ${MyMath.max(10, 5, 18, 3, 25, 12)}`);
+console.log(`MyMath.min(12, 7, 3, 9): ${MyMath.min(12, 7, 3, 9)}`);
